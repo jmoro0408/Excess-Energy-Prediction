4d76f49096f87453783794304072ca9ca15a90ab
jmoro0408 <james.moro.11@gmail.com>
2023-02-22 19:46:06 +0000

    added write_to_blob_csv fnc

--- gcp_interaction.py
+++ gcp_interaction.py
@@ -1,6 +1,8 @@
 from io import StringIO
 from google.cloud import storage
 import pandas as pd
+from pathlib import Path
+from typing import Union
 
 def list_buckets(project_id:str) -> list[str]:
     storage_client = storage.Client(project=project_id)
@@ -8,36 +10,40 @@ def list_buckets(project_id:str) -> list[str]:
     buckets = storage_client.list_buckets()
     print("Buckets:")
     for bucket in buckets:
-        print(bucket.name)
         bucket_name_list.append(bucket.name)
     return bucket_name_list
 
 def list_blobs(project_id:str, bucket_name:str) -> list[str]:
     storage_client = storage.Client(project=project_id)
     blob_name_list = []
-    print("Blobs:")
-    blobs = bucket_name.list_blobs()
+    blobs = storage_client.list_blobs(bucket_or_name = bucket_name)
     for blob in blobs:
-        print(blob.name)
         blob_name_list.append(blob.name)
     return blob_name_list
 
-def write_blob(bucket_name:str, blob_name:str):
-    """Write to a blob from GCS using file-like IO"""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
+def write_to_blob_csv(bucket_name:str,
+                      df:pd.DataFrame,
+                      filepath:Union[str,Path]) -> None:
+    """Writes a pandas df to a gcp blob as a csv.
 
-    # The ID of your new GCS object
-    # blob_name = "storage-object-name"
+    Args:
+        bucket_name (str): Name of bucket to write to
+        df (pd.DataFrame): DataFrame to write
+        filename (Union[str,Path]): full filepath of the file to be written.
+        e.g Raw_Data/EirGrid/ALL/demandActual/2017/test_data.csv
+        If the folder being written to does not currently exist it will be created.
 
+    Returns:
+        None
+    """
+    filepath = Path(filepath)
+    if filepath.suffix != '.csv':
+        filepath = filepath.with_suffix('.csv')
     storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-
-    # Mode can be specified as wb/rb for bytes mode.
-    # See: https://docs.python.org/3/library/io.html
-    with blob.open("w") as f:
-        f.write("Hello world")
+    bucket = storage_client.get_bucket(bucket_name)
+    bucket.blob(str(filepath)).upload_from_string(df.to_csv(index = False), 'text/csv')
+    print(f'{filepath} uploaded to {bucket}')
+    return None
 
 
 
